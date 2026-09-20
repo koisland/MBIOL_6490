7e7bb46ea784e4d4d1656b3a954ad5e076846126
Keith Oshima <koshima789@gmail.com>
2026-09-19 22:13:32 -0600

    Finish PS1 but some more to do.

--- src/problem_sets/PS1_Fall26_KeisukeOshima.R
+++ src/problem_sets/PS1_Fall26_KeisukeOshima.R
@@ -21,6 +21,7 @@ p_fly_samples_min <- 1 / 9
 ## 1A. 6pts. What kind of variables are (1) whether the fly samples the food during a given minute 
 ## and (2) length of its proboscis?
 # [Provide the kinds of variable]
+# TODO:
 typ_whether_fly_samples_during_min <- "boolean"
 typ_length_of_proboscis <- "float"
 
@@ -59,6 +60,7 @@ max_elapsed_time <- 14
 # [Provide the answers you obtain by running your code]
 # We iterate by minute (each interval) and calculate the probability
 # As time passes, the probability of the fly sampling the food during any given interval increases.
+# TODO: intervals
 for (elapsed_time in seq(max_elapsed_time)) {
   calc_print_prob_fly_samples(elapsed_time)
 }
@@ -70,27 +72,76 @@ for (elapsed_time in seq(max_elapsed_time)) {
 ## It takes 20 or more droplets to catch the disease.
 ## Assuming the droplets are randomly distributed among the 67 students, use R to figure out how many of Professor Joson’s students are at risk of getting sick.
 
+n_students <- 67
+n_droplets <- 1000
+thr_droplets_sick <- 20
+avg_n_droplets_per_student <- n_droplets / n_students 
+
 ## 2A. 6pts. What type of probability distribution will you use to model these data?
 #  [Provide the name of the distribution]
+typ_dist_2c <- "poisson"
 
 ## 2B. 7pts code, 1pt answer. 10pts How many of Professor Jason’s students are at risk of getting sick? 
-[Provide the annotated R code that you use to answer the question]
-# [Provide the answer you obtain by running your code]
 
+# [Provide the answer you obtain by running your code]
+# This is the probability for a single student having 20 particles or more
+prob_sick <- 1 - ppois(thr_droplets_sick - 1, avg_n_droplets_per_student)
+
+# Multiplying by the number of students gives the total number sick
+n_sick <- n_students * prob_sick
+print(
+  paste0(round(n_sick), " students (", n_sick, ") are at risk of being sick.")
+)
 
 ###
 ### Question 3
 ### 16 pts total
 ## A genomics experiment has identified genes involved in brain development. 6 encode transcription factors and 9 encode signaling proteins.
+n_genes_transcription_factors <- 6
+n_genes_signaling_proteins <- 9
 
 ## 3A. 7pts code, 1pt answer. If exactly 3 of the 15 genes are selected at random for further analysis, what is the probability that they are all transcription factors? 
-[Provide the annotated R code that you use to answer the question]
+
+n_genes_selected_3a <- 3
+n_exact_genes_transcription_factors_3a <- 3
+
+res_3a <- dhyper(
+  n_exact_genes_transcription_factors_3a,
+  n_genes_transcription_factors,
+  n_genes_signaling_proteins,
+  n_genes_selected_3a,
+)
+print(paste0(
+  "The probability that all ",
+  n_genes_selected_3a,
+  " selected genes encode transcription factors ",
+  "is ",
+  res_3a, "."
+))
+
 # [Provide the answer you obtain by running your code]
 
 ## 3B. 7pts code, 1pt answer. If exactly 5 of the 15 genes are selected at random for further analysis, what is the probability that at least 3 of them are signaling molecules? 
-[Provide the annotated R code that you use to answer the question]
-# [Provide the answer you obtain by running your code]
 
+# [Provide the answer you obtain by running your code]
+n_genes_selected_3b <- 5
+
+# 3 or more
+n_genes_or_more_signaling_proteins_3b <- 3
+# phyper part is 3 or fewer. sub by 1 to get or more
+res_3b <- 1 - phyper(
+  n_genes_or_more_signaling_proteins_3b - 1,
+  n_genes_signaling_proteins,
+  n_genes_transcription_factors,
+  n_genes_selected_3b
+)
+print(paste0(
+  "The probability that at least ",
+  n_genes_selected_3a,
+  " selected genes encode signaling proteins ",
+  "is ",
+  res_3b, "."
+))
 
 ###
 ### Question 4
@@ -98,17 +149,44 @@ for (elapsed_time in seq(max_elapsed_time)) {
 ## Submitting a grant to the NIH results in funding 4% of the time, while submitting to the State of Utah is successful 35% of the time.
 ## In your graduate career, you submit 12 grants to the NIH and 7 to the State of Utah
 
+prob_nih_grant_funding <- 0.04
+prob_utah_grant_funding <- 0.35
+n_nih_grants <- 12
+n_utah_grants <- 7
+
 ## 4A. 7pts code, 1pt answer. Use R to calculate each of the probabilities of having from 0 to 12 NIH grants funded.
-[Provide the annotated R code that you use to answer the question]
+
 # [Provide the answers you obtain by running your code]
+for (n in seq(n_nih_grants + 1)) {
+  n <- n - 1
+  res_4a_n <- dbinom(n, n_nih_grants, prob_nih_grant_funding)
+  print(paste0("The probability that ", n, " of ", n_nih_grants, " NIH grants will be funded is ", res_4a_n, "."))
+}
 
 ## 4B. 7pts code, 1pt answer. Use R to calculate each of the probabilities of having from 0 to 7 grants funded by the State of Utah.
-[Provide the annotated R code that you use to answer the question]
+
 # [Provide the answers you obtain by running your code]
+for (n in seq(n_utah_grants + 1)) {
+  n <- n - 1
+  res_4b_n <- dbinom(n, n_utah_grants, prob_utah_grant_funding)
+  print(paste0("The probability that ", n, " of ", n_utah_grants, " Utah grants will be funded is ", res_4b_n, "."))
+}
 
 ## 4C. 7pts code, 1pt answer. Using R code, calculate the probability you end up getting more grants funded by the State of Utah than by NIH.
-[Provide the annotated R code that you use to answer the question]
+
 # [Provide the answer you obtain by running your code]
+for (n in seq(n_utah_grants + 1)) {
+  n <- n - 1
+  prob_utah_grant <- dbinom(n, n_utah_grants, prob_utah_grant_funding)
+  # Less than n so need to subtract 1
+  prob_nih_lt_n_utah_grant <- pbinom(n-1, n_nih_grants, prob_nih_grant_funding)
+  # Probabilty we get n utah grants and less than n nih grants
+  prob_joint <- prob_utah_grant * prob_nih_lt_n_utah_grant
+
+  print(paste0(
+    "The probability we get ", n, " Utah grants and less than ", n, " NIH grants is ", prob_joint, "."
+  ))
+}
 
 
 ###
@@ -118,30 +196,56 @@ for (elapsed_time in seq(max_elapsed_time)) {
 ## The following data reports the cost at a sample of 25 trucks
 ##  15.76, 19.56, 20.98, 20.98, 19.71, 16.05, 22.73, 16.93, 14.58, 19.47, 18.78, 15.91, 22, 22.45, 16.78, 17.49, 18.43, 19.21, 19.29, 21.09, 20.15, 18.14, 23.74, 20.03, 22.76
 
+sample_food_truck_costs <- c(
+  5.76, 19.56, 20.98, 20.98, 19.71, 16.05, 22.73, 16.93, 14.58, 19.47, 18.78, 15.91, 22, 22.45, 16.78, 17.49, 18.43, 19.21, 19.29, 21.09, 20.15, 18.14, 23.74, 20.03, 22.76
+)
+n_sample_food_trucks <- length(sample_food_truck_costs)
+
 ## 5A-G. Using R, calculate the following statistics for the sample: mean, 
 ##  standard deviation, standard error of the mean, 2 standard errors of the 
 ##  mean, 95% confidence interval, and 90% confidence interval for the sample.
 
 # 5A. 12pts. Provide the annotated R code that you use to calculate the statistics.
-[Provide the annotated R code that you use to calculate all the statistics]
+mean_5ag <- mean(sample_food_truck_costs)
+stdev_5ag <- sd(sample_food_truck_costs)
+sem_5ag <- stdev_5ag / sqrt(n_sample_food_trucks)
+# TODO:
+two_sem_5ag <- sem_5ag * 2
+
+# TODO: Use qnorm?
+critical_value_90 <- 1.645
+critical_value_95 <- 1.96
+
+ci_95_l_5ag <- mean_5ag - critical_value_95 * sem_5ag
+ci_95_r_5ag <- mean_5ag + critical_value_95 * sem_5ag
+ci_90_l_5ag <- mean_5ag - critical_value_90 * sem_5ag
+ci_90_r_5ag <- mean_5ag + critical_value_90 * sem_5ag
 
 ## For 5B-G, provide the answers you calculated with your code. 1 pt for each answer.
 ## 5B. mean
 # [Provide your answer]
+stmt_5ag <- " of food truck samples is "
+print(paste0("Mean", stmt_5ag, mean_5ag, "."))
 
 ## 5C. standard deviation
 # [Provide your answer]
+print(paste0("Standard deviation", stmt_5ag, stdev_5ag, "."))
 
 ## 5D. standard error of the mean
 # [Provide your answer]
+print(paste0("Standard error of mean", stmt_5ag, sem_5ag, "."))
 
 ## 5E. 2 standard errors of the mean
 # [Provide your answer]
+print(paste0("Two standard errors of mean", stmt_5ag, two_sem_5ag, "."))
 
 ## 5F. 95% confidence interval
 # [Provide your answer]
+print(paste0("The 95% confidence interval", stmt_5ag, ci_95_l_5ag, " and ", ci_95_r_5ag, "."))
+# TODO: Why different?
+# t.test(sample_food_truck_costs, mu=mean_5ag, conf.level = 0.95)$conf.int
 
 ## 5G. 90% confidence interval
-# [Delete this line and replace with your answer]
-
-
+print(paste0("The 90% confidence interval", stmt_5ag, ci_90_l_5ag, " and ", ci_90_r_5ag, "."))
+# TODO: Why different?
+# t.test(sample_food_truck_costs, mu=mean_5ag, conf.level = 0.9)$conf.int
